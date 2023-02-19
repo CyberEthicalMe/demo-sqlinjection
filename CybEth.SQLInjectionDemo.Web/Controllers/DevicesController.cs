ae1b4bd143918aa346d606f1a731de61ea6b00f3
Kamil Gierach-Pacanek <20944885+KamilPacanek@users.noreply.github.com>
2023-02-19 12:12:33 +0100

    Change code to show devices instead weather forecast on Home

---
+++ CybEth.SQLInjectionDemo.Web/Controllers/DevicesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CybEth.SQLInjectionDemo.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DevicesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<DevicesController> _logger;
+
+        public DevicesController(ILogger<DevicesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Device> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Device
+            {
+                Id = index,
+                Name = Random.Shared.Next().ToString(),
+                Price = Random.Shared.Next().ToString()
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ CybEth.SQLInjectionDemo.Web/Device.cs
@@ -0,0 +1,11 @@
+namespace CybEth.SQLInjectionDemo.Web
+{
+    public class Device
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Price{ get; set; }
+    }
+}
